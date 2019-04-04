057912bc7c69e12cc27827f90810799c8fe6ba22
Awesome20o0 <varungorti@gmail.com>
2019-04-04 09:40:33 -0500

    Changed some setpoints from level two to ensure that going down to be safe with the hatch mech does not cause carriage to hit into the intake

--- 2019 Comp Season Code/src/main/cpp/commands/ElevatorMacros/Region2/ElevatorTwoMedInMacro.cpp
+++ 2019 Comp Season Code/src/main/cpp/commands/ElevatorMacros/Region2/ElevatorTwoMedInMacro.cpp
@@ -3,6 +3,8 @@
 
 ElevatorTwoMedInMacro::ElevatorTwoMedInMacro()
 {
+    AddSequential(new SetIntakeMacro(true));
+    AddSequential(new WaitCommand(.2));
     AddSequential(new SetElevatorSetpointDefinite(Robot::m_elevator.kLiftBarTopSafe));
     AddSequential(new SetHatch(true));
     AddSequential(new WaitCommand(.3));
